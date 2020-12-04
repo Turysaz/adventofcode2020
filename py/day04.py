fba387a3e5b4c1e3e4dca5a3f86139bcec5e4b9b
turysaz <turysaz@posteo.org>
2020-12-04 09:39:14 +0100

    [py] Some prettifying on day 04

--- py/day04.py
+++ py/day04.py
@@ -13,68 +13,53 @@ def split_data(text: str) -> list:
             latest = "" # reset
     yield latest # last line
 
-def part1():
-    print("Part 1")
-    fieldbits = {
-        "byr" : 0x01,
-        "iyr" : 0x02,
-        "eyr" : 0x04,
-        "hgt" : 0x08,
-        "hcl" : 0x10,
-        "ecl" : 0x20,
-        "pid" : 0x40,
-        "cid" : 0x80
-    }
-    req_mask = 0x7f
-    c_valid = 0
-    for data in split_data(text):
-        checksum = 0
-        for pair in re.finditer(r"(?P<key>\w{3}):(?P<val>\w*)", data):
-            checksum += fieldbits[pair.group("key")]
-        if (checksum & req_mask) == req_mask:
-             c_valid += 1
+def int_in_range(x: str, min: int, max: int, l: int) -> bool:
+    return (min <= int(x) <= max) if x.isdigit() and len(x) == l else False
 
-    print ("Valid: {}".format(c_valid))
+def hgt_is_valid(x: str) -> bool:
+    if x[-2:] == "cm":
+        return int_in_range(x[:-2], 150, 193, 3)
+    elif x[-2:] == "in":
+        return int_in_range(x[:-2], 59, 76, 2)
+    else:
+        return False
 
-def part2():
-    print("Part 2")
+checkhandler_part1 = {
+    "byr" : lambda x : 0x01,
+    "iyr" : lambda x : 0x02,
+    "eyr" : lambda x : 0x04,
+    "hgt" : lambda x : 0x08,
+    "hcl" : lambda x : 0x10,
+    "ecl" : lambda x : 0x20,
+    "pid" : lambda x : 0x40,
+    "cid" : lambda x : 0x80
+}
 
-    def intbetween(x: str, min: int, max: int, length: int) -> bool:
-        if not x.isdigit() or len(x) != length:
-            return False
-        
-        return int(x) >= min and int(x) <= max
+checkhandler_part2 = {
+    "byr" : lambda x : 0x01 if int_in_range(x, 1920, 2002, 4) else 0,
+    "iyr" : lambda x : 0x02 if int_in_range(x, 2010, 2020, 4) else 0,
+    "eyr" : lambda x : 0x04 if int_in_range(x, 2020, 2030, 4) else 0,
+    "hgt" : lambda x : 0x08 if hgt_is_valid(x) else 0,
+    "hcl" : lambda x : 0x10 if re.match(r"\#[0-9a-f]{6}", x) != None else 0,
+    "ecl" : lambda x : 0x20 if x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"] else 0,
+    "pid" : lambda x : 0x40 if len(x) == 9 and x.isdigit() else 0,
+    "cid" : lambda x : 0x80
+}
 
-    def hgtvalid(x: str) -> bool:
-        if x[-2:] == "cm":
-            return intbetween(x[:-2], 150, 193, 3)
-        elif x[-2:] == "in":
-            return intbetween(x[:-2], 59, 76, 2)
-        else:
-            return False
-
-    checkhandler = {
-        "byr" : lambda x : 0x01 if intbetween(x, 1920, 2002, 4) else 0,
-        "iyr" : lambda x : 0x02 if intbetween(x, 2010, 2020, 4) else 0,
-        "eyr" : lambda x : 0x04 if intbetween(x, 2020, 2030, 4) else 0,
-        "hgt" : lambda x : 0x08 if hgtvalid(x) else 0,
-        "hcl" : lambda x : 0x10 if re.match(r"\#[0-9a-f]{6}", x) != None else 0,
-        "ecl" : lambda x : 0x20 if x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"] else 0,
-        "pid" : lambda x : 0x40 if len(x) == 9 and x.isdigit() else 0,
-        "cid" : lambda x : 0x80
-    }
-
+def count_valid(checkhandler: dict):
     req_mask = 0x7f
     valid = 0
     for data in split_data(text):
-        checksum = 0
-        for pair in re.finditer(r"(?P<key>\w{3}):(?P<val>[^\s]*)", data):
-            k = pair.group("key")
-            v = pair.group("val")
-            checksum += checkhandler[k](v)
+        checksum = sum(
+            set(checkhandler[match.group("key")](match.group("val"))
+                for match
+                in re.finditer(r"(?P<key>\w{3}):(?P<val>[^\s]*)", data)))
         if (checksum & req_mask) == req_mask:
             valid += 1
     print ("Valid: {}".format(valid))
 
-part1()
-part2()
+print("Part 1")
+count_valid(checkhandler_part1)
+
+print("Part 2")
+count_valid(checkhandler_part2)
