3a62d11e7cc4e533bb6fd17909da6f398b96e2dc
turysaz <turysaz@posteo.org>
2020-12-05 15:18:37 +0100

    [py] Condense day 05 even more

--- py/day05.py
+++ py/day05.py
@@ -2,25 +2,18 @@
 with open("../inputs/day05.txt") as file:
     lines = file.read().splitlines(keepends=False)
 
-
 def get_id(line: str) -> int:
     x = 0
     for c in line:
-        x <<= 1
-        x += 1 if c in "RB" else 0
+        x = (x << 1) | int(c in "RB")
     return x
 
 ids = [get_id(line) for line in lines]
-_max = max(ids)
-
-print("Part 1")
-print(_max)
-
-print("Part 2")
-
 ids.sort()
+
+print("Part 1: %i" % ids[-1])
+
 for i in range(len(ids) - 2): # -1 because of start index 0, -1 for lookahead
     if ids[i] + 2 == ids[i+1]: # + 2 to find gap
-        print(ids[i] + 1) # get gap
+        print("Part 2: %i" % (ids[i] + 1)) # get gap
         exit()
-
