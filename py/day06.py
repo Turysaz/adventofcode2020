5adb51a0d1b395cf1dffdb3dc835a1e7b270e9ac
Turysaz <turysaz@posteo.org>
2020-12-06 18:15:44 +0100

    [py] Rework day 06 to be shorter and more elegant

--- py/day06.py
+++ py/day06.py
@@ -1,34 +1,24 @@
 with open("../inputs/day06.txt") as file:
     lines = file.read().splitlines(keepends=False)
 
-def split_data(lines: list) -> list:
-    latest, size = {}, 0
+def get_groups(lines: list) -> list:
+    group = []
     for line in lines:
         if line == "":
-            yield (latest, size)
-            latest, size = {}, 0
+            yield group
+            group = []
             continue
-
-        size += 1
-
-        for c in line:
-            if c in latest:
-                latest[c] += 1
-            else:
-                latest[c] = 1
-
-    yield (latest, size) # last line
-
-def part1():
-    return sum(len(group[0]) for group in split_data(lines))
+        group.append(line)
+    yield group # last line
 
 def part2():
-    s = 0
-    for group in split_data(lines):
-        for ans in group[0]:
-            s += int(group[0][ans] == group[1])
-    return s
+    return sum( # sums up the common chars in *all* groups
+        sum(1 for _ # sums up the common chars in g
+            in filter(
+                # get all elements within the *shortest* line that are in *all others* also
+                lambda c: all([c in answers for answers in g]),
+                min(g, key=len)))
+        for g in get_groups(lines))
 
-print("Part 1: %i" % part1())
+print("Part 1: %i" % sum(len(set("".join(g))) for g in get_groups(lines)))
 print("Part 2: %i" % part2())
-
