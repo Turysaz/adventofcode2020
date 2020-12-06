b7c5704ceacb4ea8836e9dfdddb49616690f41af
Turysaz <turysaz@posteo.org>
2020-12-06 18:24:32 +0100

    [py] Shrink day06 a little more

--- py/day06.py
+++ py/day06.py
@@ -11,14 +11,11 @@ def get_groups(lines: list) -> list:
         group.append(line)
     yield group # last line
 
-def part2():
-    return sum( # sums up the common chars in *all* groups
+print("Part 1: %i" % sum(len(set("".join(g))) for g in get_groups(lines)))
+print("Part 2: %i" % sum( # sums up the common chars in *all* groups
         sum(1 for _ # sums up the common chars in g
             in filter(
                 # get all elements within the *shortest* line that are in *all others* also
                 lambda c: all([c in answers for answers in g]),
                 min(g, key=len)))
-        for g in get_groups(lines))
-
-print("Part 1: %i" % sum(len(set("".join(g))) for g in get_groups(lines)))
-print("Part 2: %i" % part2())
+        for g in get_groups(lines)))
