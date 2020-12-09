189265e76f93abb9560846e7b90d8de4bdc21707
turysaz <turysaz@posteo.org>
2020-12-09 07:20:50 +0100

    [py] Make day09 a little prettier

--- py/day09.py
+++ py/day09.py
@@ -1,9 +1,8 @@
-
 from itertools import combinations
 with open("../inputs/day09.txt") as file:
     numbers = [int(line) for line in file.readlines()]
 
-def crackXMAS(numbers, rot=25):
+def find_weak(numbers, rot=25):
     target = 0
     buffer = numbers[:rot]
     for j in range(len(numbers) - rot):
@@ -12,17 +11,23 @@ def crackXMAS(numbers, rot=25):
         if not any(map(
                 lambda x : (x[0] != x[1] and sum(x) == target),
                 combinations(buffer, 2))):
-            print("weakness: %i" % target)
-            break
+            return target
         buffer[i%rot] = target
+
+def find_seq(numbers, weak):
     chunklen = 2
     while(chunklen < len(numbers)):
         for j in range(len(numbers) - chunklen):
             buffer = numbers[j:chunklen+j]
-            if(sum(buffer) == target):
-                _min, _max = min(buffer), max(buffer)
-                print("Seq: {}, Min: {}, Max:{}, Sum:{}".format(buffer, _min, _max, _min+_max))
-                return
+            if(sum(buffer) == weak):
+                return buffer
         chunklen += 1
+
+def crackXMAS(numbers, rot=25):
+    weak = find_weak(numbers, rot)
+    print("Weak: {}".format(weak))
+    seq = find_seq(numbers, weak)
+    _min, _max = min(seq), max(seq)
+    print("Seq: {}, Min: {}, Max:{}, Sum:{}".format(seq, _min, _max, _min+_max))
 
 crackXMAS(numbers, 25)
