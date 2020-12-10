2bb0b49817272b032fe94409c3f17ef2986be5a6
turysaz <turysaz@posteo.org>
2020-12-10 17:55:23 +0100

    [py] Compress day10

--- py/day10.py
+++ py/day10.py
@@ -1,23 +1,23 @@
 from math import prod
 with open("../inputs/day10.txt") as file:
-    adapters = sorted([int(l) for l in file.readlines()])
+    jolts = sorted([int(l) for l in file.readlines()])
 
-def solve(jolts):
-    def f(x):
-        if(x <= 1): return 1
-        if(x == 2): return 2
-        return f(x-3) + f(x-2) + f(x-1)
-    jolts = [0] + jolts + [jolts[-1] + 3] #prepend 0 and append +3 for device
-    one_sequences = []
-    current = 0
-    for i in range(len(jolts) - 1):
-        if(jolts[i+1] - jolts[i] == 1):
-            current += 1
-        elif(jolts[i+1] - jolts[i] == 3):
-            if(current > 0):
-                one_sequences.append(current)
-                current = 0
-    print((len(jolts) - 1 - sum(one_sequences)) * sum(one_sequences))
-    print(prod(map(f, one_sequences)))
+# a "sequence" is a consecutive row of 'single steps' (1 step vs. 3 steps)
+def f(x: int) -> int:
+    "Gets the possible paths through a sequence of length x"
+    if(x <= 2): return max(x, 1)
+    return f(x-3) + f(x-2) + f(x-1)
 
-solve(adapters)
+jolts = [0] + jolts + [jolts[-1] + 3] #prepend 0 and append +3 for device
+sequences, current_seq = [], 0 # get all sequences by their lengths
+for i in range(len(jolts) - 1):
+    if(jolts[i+1] - jolts[i] == 1):
+        current_seq += 1
+        continue
+    if(current_seq > 0): # end of sequence
+        sequences.append(current_seq)
+        current_seq = 0
+
+print("Part 1 %i" % ((len(jolts)-1 - sum(sequences)) * sum(sequences)))
+print("Part 2 %i" % prod(map(f, sequences)))
+
