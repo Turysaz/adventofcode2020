412a344a61b3cdd3e2a2fe8db3d025dd7c75350a
turysaz <turysaz@posteo.org>
2020-12-11 19:20:56 +0100

    [py] Complete day11

--- py/day11.py
+++ py/day11.py
@@ -13,29 +13,32 @@ grid_d = """#.##.##.##
 #.######.#
 #.#####.##""".splitlines(keepends=False)
 
-def gsc(g,x,y): #get state char
-    if -1 >= x or x >= len(g[0]): return "."
-    if -1 >= y or y >= len(g): return "."
+def get_seat(g,x,y): #get state char
+    if -1 >= x or x >= len(g[0]): return "e"
+    if -1 >= y or y >= len(g): return "e"
     return g[y][x]
 
 # mapping
-def new_state(g,x,y):
-    def gs(x,y):
-        m={"L" : -1, "." : -1, "#" : +1}
-        return m[gsc(g,x,y)]
-
-    if gsc(g,x,y) == ".":
+def new_state(g,x,y,tolerance,recurse):
+    def neighbor_taken(base, vec, recurse):
+        _x, _y = base[0] + vec[0], base[1] + vec[1]
+        v = get_seat(g, _x, _y)
+        if v == "e":
+            return -1
+        if v == ".":
+            return -1 if not recurse else neighbor_taken((_x, _y), vec, recurse)
+        return 1 if v == "#" else -1
+    
+    if get_seat(g, x, y) == ".":
         return "."
-    new = (
-        gs(x-1,y-1) + gs(x+0,y-1) + gs(x+1,y-1) +
-        gs(x-1,y+0) +           0 + gs(x+1,y+0) +
-        gs(x-1,y+1) + gs(x+0,y+1) + gs(x+1,y+1))
+    directions = [(-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0)]
+    new = sum([neighbor_taken((x, y), d, recurse) for d in directions])
 
     if new == -8: return "#"
-    if new >= 0: return "L"
-    return gsc(g,x,y)
+    if new >= tolerance - 4: return "L"
+    return get_seat(g,x,y)
 
-def run_to_stable(g):
+def run_to_stable(g, tolerance, recurse):
     changed = True
     rounds = 0
     while changed:
@@ -45,8 +48,8 @@ def run_to_stable(g):
         for y in range(len(g)):
             newline = ""
             for x in range(len(g[0])):
-                old = gsc(g,x,y)
-                new = new_state(g,x,y)
+                old = get_seat(g,x,y)
+                new = new_state(g,x,y, tolerance, recurse)
                 if(new != old):
                     changed = True
                 newline += new
@@ -57,7 +60,13 @@ def run_to_stable(g):
     occupied = 0
     for l in g:
         occupied += l.count("#")
-        print(l)
+        #print(l)
     print("TakenSeats: %i" % occupied)
 
-run_to_stable(grid)
+# 1: 37  2166
+# 2: 26
+
+run_to_stable(grid_d,4,False)
+run_to_stable(grid_d,5,True)
+run_to_stable(grid,4,False)
+run_to_stable(grid,5,True)
