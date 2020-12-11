65a1761d015dbe0373c0cd2d58dc35a346ecb2bb
turysaz <turysaz@posteo.org>
2020-12-11 19:59:30 +0100

    [py] Cleanup day11

--- py/day11.py
+++ py/day11.py
@@ -1,72 +1,56 @@
 from itertools import combinations
 with open("../inputs/day11.txt") as file:
-    grid = file.read().splitlines(keepends=False)
+    seat_map = file.read().splitlines(keepends=False)
 
-grid_d = """#.##.##.##
-#######.##
-#.#.#..#..
-####.##.##
-#.##.##.##
-#.#####.##
-..#.#.....
-##########
-#.######.#
-#.#####.##""".splitlines(keepends=False)
-
-def get_seat(g,x,y): #get state char
-    if -1 >= x or x >= len(g[0]): return "e"
-    if -1 >= y or y >= len(g): return "e"
-    return g[y][x]
+def get_seat(grid, x, y):
+    if -1 >= x or x >= len(grid[0]): return "x" # "x" marks out of range
+    if -1 >= y or y >= len(grid): return "x"
+    return grid[y][x]
 
 # mapping
-def new_state(g,x,y,tolerance,recurse):
-    def neighbor_taken(base, vec, recurse):
-        _x, _y = base[0] + vec[0], base[1] + vec[1]
-        v = get_seat(g, _x, _y)
-        if v == "e":
-            return -1
-        if v == ".":
-            return -1 if not recurse else neighbor_taken((_x, _y), vec, recurse)
-        return 1 if v == "#" else -1
+def new_state(grid, x, y, tolerance, recurse):
+    """
+    Gets the new value of a grid cell considering the tolerance of other people
+    before leaving the seat.
+    The "recurse" option switches between direct neighbors and first seat in
+    line of sight.
+    """
+    def relative_taken(base, offset, recurse):
+        n_x, n_y = base[0] + offset[0], base[1] + offset[1]
+        n_state = get_seat(grid, n_x, n_y)
+        if n_state == "x":
+            return False
+        if n_state == ".":
+            return False if not recurse else relative_taken((n_x, n_y), offset, recurse)
+        return n_state == "#"
     
-    if get_seat(g, x, y) == ".":
+    if get_seat(grid, x, y) == ".":
         return "."
-    directions = [(-1,-1), (0,-1), (1,-1), (1,0), (1,1), (0,1), (-1,1), (-1,0)]
-    new = sum([neighbor_taken((x, y), d, recurse) for d in directions])
 
-    if new == -8: return "#"
-    if new >= tolerance - 4: return "L"
-    return get_seat(g,x,y)
+    directions = [(x,y) for x in [-1,0,1] for y in [-1,0,1] if x != 0 or y != 0]
+    taken_l = [relative_taken((x, y), d, recurse) for d in directions]
+    taken = taken_l.count(True)
+    if taken == 0: return "#"
+    if taken > tolerance: return "L"
+    return get_seat(grid,x,y)
 
 def run_to_stable(g, tolerance, recurse):
     changed = True
-    rounds = 0
     while changed:
-        rounds += 1
         changed = False
         newgrid = []
         for y in range(len(g)):
             newline = ""
             for x in range(len(g[0])):
-                old = get_seat(g,x,y)
-                new = new_state(g,x,y, tolerance, recurse)
+                old = get_seat(g, x, y)
+                new = new_state(g, x, y, tolerance, recurse)
                 if(new != old):
                     changed = True
                 newline += new
             newgrid.append(newline)
         g = newgrid
 
-    print("stable after %i rounds:" % rounds)
-    occupied = 0
-    for l in g:
-        occupied += l.count("#")
-        #print(l)
-    print("TakenSeats: %i" % occupied)
+    return sum([l.count("#") for l in g])
 
-# 1: 37  2166
-# 2: 26
-
-run_to_stable(grid_d,4,False)
-run_to_stable(grid_d,5,True)
-run_to_stable(grid,4,False)
-run_to_stable(grid,5,True)
+print("Part 1: %i" % run_to_stable(seat_map, 3, False))
+print("Part 2: %i" % run_to_stable(seat_map, 4, True))
