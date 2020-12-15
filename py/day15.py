7d59abe3c0881ecf96b685b6e15cb3aed3013c50
turysaz <turysaz@posteo.org>
2020-12-15 15:24:37 +0100

    [py] Complete day15

---
+++ py/day15.py
@@ -0,0 +1,23 @@
+
+start =[6,4,12,1,20,0,16]
+
+def get_nth_number(n: int) -> int:
+    last_time_of = {v:start.index(v)+1 for v in start[:-1]}
+    turn = len(start)
+    last_spoken = start[-1]
+
+    while turn < n:
+        if last_spoken not in last_time_of:
+            speak = 0
+        else:
+            speak = turn - last_time_of[last_spoken]
+        last_time_of[last_spoken] = turn
+        last_spoken = speak
+        turn += 1
+
+    return last_spoken
+
+print("Part 1: %i" % get_nth_number(2020))
+print("Part 2: %i" % get_nth_number(30000000))
+
+
