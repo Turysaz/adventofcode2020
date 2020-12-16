ad2d0a03a81573ea043f8e0765a2395af673628f
turysaz <turysaz@posteo.org>
2020-12-16 21:30:25 +0100

    [py] Compress day16

--- py/day16.py
+++ py/day16.py
@@ -4,6 +4,9 @@
 # Tasks:
 # Part 1: In sets of numbers, find numbers that do not match any of the given
 #         rules (ranges) and return the sum of them.
+# Part 2: Determine which position of numbers must be which field and then
+#         multiply together all numbers from the own ticket that belong to
+#         fields that start with "departure".
 #
 
 import re
@@ -48,39 +51,34 @@ if __name__ == "__main__":
 
     rules, own_ticket, other_tickets = parse(lines)
 
-    valid_tix = [] # entries represent the matches for one ticket, each.
+    valid_tickets = [] # entries represent the matches for one ticket, each.
     error_rate = 0
-    other_tickets.append(own_ticket)
     for ticket in other_tickets:
-        field_matches = []
+        applying_rules_for_fields = []
         error, errorcode = False, 0
-        for candidate in ticket:
-            applying_rules = set(r.name for r in rules if r.okay(candidate))
+        for number in ticket:
+            applying_rules = set(r.name for r in rules if r.okay(number))
             if len(applying_rules) == 0:
                 error = True
-                errorcode += candidate
-            field_matches.append(applying_rules)
+                errorcode += number
+            applying_rules_for_fields.append(applying_rules)
         error_rate += errorcode
         if not error:
-            valid_tix.append(field_matches)
+            valid_tickets.append(applying_rules_for_fields)
 
     print("Part 1: %i" % error_rate)
 
-    candidates=[]
-    for i in range(len(rules)):
-        candidates.append(list(set.intersection(*[t[i] for t in valid_tix])))
-
-    field_indices = {}
-    while (candidate := next(filter(lambda x: len(x) == 1, candidates), False)):
-        field_name = candidate[0]
-        field_indices[candidates.index(candidate)] = field_name
-        for c in candidates:
-            if field_name in c:
-                c.remove(field_name)
-
     product = 1
-    for i in field_indices:
-        if field_indices[i].startswith("departure"):
-            product *= own_ticket[i]
+    candidates = [list(set.intersection(*[t[i] for t in valid_tickets]))
+                  for i in range(len(rules))]
+    # iteratively remove the unambigous field from all sets to reduce the
+    # number of possibilities
+    while (candidate := next(filter(lambda x: len(x) == 1, candidates), False)):
+        name = candidate[0]
+        product *= (own_ticket[candidates.index(candidate)]
+                    if name.startswith("departure") else 1)
+        for c in candidates:
+            if name in c:
+                c.remove(name)
 
     print("Part 2: %i" % product)
